7cba64249094d7d3278ec45f50cae305b1bee43e
echpo4mak <lesha_ostrovskiy@mail.ru>
2020-04-04 15:40:56 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -18,7 +18,7 @@ sessionStorage = {}
 
 
 # Задаем параметры приложения Flask.
-@app.route("/post", methods=['POST'])
+@app.route("/", methods=['POST'])
 def main():
     # Функция получает тело запроса и возвращает ответ.
     logging.info('Request: %r', request.json)
